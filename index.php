8d15e2d6322c5b0abee7cdfc0261a855c6854aa4
Richard Pett <rich_pett@hotmail.com>
2014-12-19 17:36:56 +0000

    index.php pull in angular js scrip and, we have added ng-app to html, which is the name of the module? and bound the data from the text input to the name property of , which is dynmicaly displayed on the page

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/> 
+<title>Title of the document</title>
+</head>
+
+<body>
+
+<?php echo 'PHP running'; ?>
+
+
+</body>
+
+</html>
