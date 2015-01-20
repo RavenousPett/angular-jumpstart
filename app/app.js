382f6beff93e50b168447f81a52f5135779856d0
Richard Pett <rich_pett@hotmail.com>
2015-01-20 13:30:13 +0000

    latest on home comp

--- app/app.js
+++ app/app.js
@@ -3,6 +3,15 @@
  */
 (function(){
 
-    angular.module('customersApp', []);
+    var app = angular.module('customersApp', ['ngRoute']);
+
+    app.config( function($routeProvider) {
+        $routeProvider
+            .when('/', {
+                controller: 'CustomersController',
+                templateUrl: 'app/views/customers.html'
+            })
+            .otherwise( { 'redirectTo': '/' });
+    });
 
 }());
--- index.php
+++ index.php
@@ -8,111 +8,14 @@
 		<link rel="stylesheet" type="text/css" href="css/main.css">
 	</head>
 
-	<body ng-init="data={name: 'James', isVisible: true, loggedIn: false, status: 'navy'}">
-		<!--
-		ng-cloak
-		ng-switch
-		ng-class
-		ng-show
-		-->
-		<div ng-cloak>
+	<body>
 
-			<h1>Customer Data</h1>
+		<div ng-view></div>
 
-			Filter: <input type="text" ng-model="customerFilter.name">
-
-			<div ng-controller="CustomersController">
-
-				<table>
-					<tr>
-						<th ng-click="doSort('name')">Name</th>
-						<th ng-click="doSort('city')">City</th>
-						<th ng-click="doSort('orderTotal')">Order Total</th>
-						<th ng-click="doSort('joined')">Joined</th>
-					</tr>
-
-					<tr ng-repeat="cust in customers | filter:customerFilter | orderBy:sortBy:reverse" >
-						<td>{{ cust.name | uppercase}}</td>
-						<td>{{ cust.city | lowercase}}</td>
-						<td>{{ cust.orderTotal | currency:'£' }}</td>
-						<td>{{ cust.joined | date:'dd-mm-yy' }}</td>
-					</tr>
-				</table>
-
-				<br />
-
-				<span>Total customers: {{ customers.length }}</span>
-
-
-			</div>
-
-			<h1>{{ 1 + 1 }}</h1>
-
-			<div ng-switch on="data.loggedIn">
-				<div ng-switch-when="true">
-					Welcome {{ data.name }}
-				</div>
-
-				<div ng-switch-default ng-class="data.status">Login</div>
-
-			</div>
-
-			<br />
-
-			<div ng-show="data.isVisible">
-				Name: <input type="text" ng-model="name">
-				<br />{{ name }}
-				<br /><span ng-bind="name"></span>
-			</div>
-<!---->
-<!--			<div ng-hide="isHidden">-->
-<!--				Name: <input type="text" ng-model="name">-->
-<!--				<br />{{ name }}-->
-<!--				<br /><span ng-bind="name"></span>-->
-<!--			</div>-->
-
-<!--			<input type="checkbox" ng-model="isHidden">-->
-
-<!--			<button ng-click="name='Michelle'">Change Name</button>-->
-
-<!--			<div ng-init="names=['David', 'James', 'Richard']">-->
-<!---->
-<!--				<h3>Interating through data with ng-repeat</h3>-->
-<!---->
-<!--				<ul>-->
-<!--					<li ng-repeat="name in names">{{ name }}</li>-->
-<!--				</ul>-->
-<!---->
-<!--			</div>-->
-
-				<div ng-init="people=[{name:'John', city:'Chandler'}, {name:'Tina', city:'Los Angeles'}, {name:'David', city:'London'}, {name:'James', city:'Bagshot'}]">
-
-					<h3>Interating through data with ng-repeat</h3>
-
-					<ul>
-						<li ng-repeat="person in people">{{ person.name }}, {{ person.city }}</li>
-<!--						<li ng-repeat="person in people">{{ person.name + ', ' + person.city }}</li>-->
-
-					</ul>
-
-					<br />
-
-					<table>
-						<tr>
-							<td>Name</td>
-							<td>City</td>
-						</tr>
-						<tr ng-repeat="person in people">
-							<td>{{ person.name }}</td>
-							<td>{{ person.city }}</td>
-						</tr>
-					</table>
-
-				</div>
-
-		</div>
-
-		<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/angularjs/1.2.28/angular.min.js"></script>
+		<!--	AngularJS	-->
+		<script type="text/javascript" src="scripts/angular.js"></script>
+		<!--	ngRoute	-->
+		<script type="text/javascript" src="scripts/angular-route.js"></script>
 		<script type="text/javascript" src="app/app.js"></script>
 		<script type="text/javascript" src="app/controllers/customersController.js"></script>
 
